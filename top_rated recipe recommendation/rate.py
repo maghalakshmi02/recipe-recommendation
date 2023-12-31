f7a69c8739ec2d87742cfc25e3f91288ad5c8000
Maghalakshmi R <136116819+maghalakshmi02@users.noreply.github.com>
2023-12-31 22:39:06 +0530

    Add files via upload

---
+++ top_rated recipe recommendation/rate.py
@@ -0,0 +1,32 @@
+import pandas as pd
+# Data Loading 
+recipe_df = pd.read_csv(r"ff.csv")
+rating_df=pd.read_csv(r"rating.csv")
+#renaming the column name
+recipe_df.rename(columns={'RecipeId':'recipe_id'},inplace=True)
+# combining the df (which users voted for recipe_id=1)
+df = pd.merge(recipe_df,rating_df,on='recipe_id')
+#checking for null values
+combined_df = df.dropna()
+#calculating no_of_user voted for each recipe--(v)
+no_of_votes=(combined_df.groupby(by=['recipe_id'])['rating'].count().reset_index().
+rename(columns = {'rating': 'totalRatingCount'})
+[['recipe_id', 'totalRatingCount']])
+#calculating avg(mean)_of_rating for each recipe--(r)
+ratings_avg=(combined_df.groupby(by=['recipe_id'])['rating'].mean().reset_index().
+rename(columns = {'rating': 'avg'})
+[['recipe_id', 'avg']])
+no_of_votes['RecipeName']=recipe_df['RecipeName']
+no_of_votes['average']=ratings_avg['avg']
+no_of_votes['URL']=recipe_df['URL']
+v=no_of_votes['totalRatingCount']#no.of votes for each recipe
+m=no_of_votes['totalRatingCount'].quantile(0.70)#minimum votes required for each recipe
+r=no_of_votes['average']#avg of the ratings for each recipe
+c=no_of_votes['totalRatingCount'].mean()#mean vote across the whole report
+#weighted_average calculation
+no_of_votes['weighted_avg']=((r*v)+(c*m))/(v+m)
+fdf=no_of_votes.set_index('recipe_id')
+#weighted Rate sorted
+recipe=fdf.sort_values('weighted_avg',ascending=False)
+lists=recipe[['RecipeName','weighted_avg','URL']]
+existing_df=lists.head()
---
+++ top_rated recipe recommendation/stt.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import rate as r
+
+# Assuming you have an existing DataFrame named existing_df
+# Extract relevant columns
+selected_columns = ['RecipeName', 'weighted_avg', 'URL']
+df = r.existing_df[selected_columns].copy()
+
+# Set 'RecipeName' column as the index
+df.set_index('RecipeName', inplace=True)
+
+# Streamlit app
+st.title('Top Rated Recipes')
+st.header('Explore and Discover Delicious Recipes')
+
+# Display the list of recipes with weighted average and URL
+st.write('Here are the recipes:')
+st.table(df[['weighted_avg']])
+
+# Additional features - you can customize the display
+selected_recipe = st.selectbox('Select a recipe to view details:', df.index)
+
+# Display details for the selected recipe
+selected_row = df.loc[selected_recipe]
+st.write(f'Details for {selected_recipe}:')
+#st.write(f'Weighted Average Rating: {selected_row["weighted_avg"]}')
+st.write(f'URL: {selected_row["URL"]}')
